ea1dcc19f37a6789921785c35724edd20f3e0efe
irzanaldi <irzanaldi@gmail.com>
2022-01-08 20:51:07 +0700

    fix typo

--- database/migrations/2022_01_08_115027_create_kesimpulans_table.php
+++ database/migrations/2022_01_08_115027_create_kesimpulans_table.php
@@ -16,7 +16,7 @@ class CreateKesimpulansTable extends Migration
         Schema::create('kesimpulans', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('solus');
+            $table->string('solusi');
             $table->foreignId('unsur_id')->references('id')->on("unsur_haras")->onDelete('cascade');
             $table->timestamps();
         });
